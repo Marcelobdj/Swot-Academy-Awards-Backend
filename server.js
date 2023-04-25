aed6f96c1467c45730b17ca54c69f27d6034890f
Marcelo Di Jorge <marcelobdij@gmail.com>
2023-04-24 21:57:16 -0300

    feat: add ensurement that the server listens for incoming connections on its public IP address

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get("/", (req, res) => {
 
 // Start server
 const PORT = process.env.PORT || 7000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
