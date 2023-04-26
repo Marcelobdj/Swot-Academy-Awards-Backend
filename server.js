03b7d8d3d86d0d00bd513a2c41a462ee1d53111b
Marcelo Di Jorge <marcelobdij@gmail.com>
2023-04-26 00:41:48 -0300

    feat: add a correction to the server.js where the useFindAndModify option should be set via mongoose.set() instead of being passed as an option in mongoose.connect()

--- server.js
+++ server.js
@@ -18,9 +18,11 @@ const connectToMongoDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false, // Add this line
+            useUnifiedTopology: true
         });
+
+        mongoose.set('useFindAndModify', false);
+
         console.log("Connected to MongoDB");
     } catch (err) {
         console.error("Error connecting to MongoDB:", err);
